318549578bd5684bdbcf0866ae4876439e519aca
Alexandru Dan <dan.alex97@yahoo.com>
2017-12-14 22:13:31 +0000

    Modified init.sh script.

--- init.sh
+++ init.sh
@@ -1,7 +1,33 @@
+#!/usr/bin/env bash
+
 set -e pipefail
 
-mkdir -p datasets
-cd datasets
-wget -c http://www.da.inf.ethz.ch/files/twitter-datasets.zip
-unzip -o twitter-datasets.zip
-cd ../
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+DATA_DIR=$DIR/datasets
+
+function download_dataset() {
+	echo "Downlading dataset..."
+	mkdir -p $DATA_DIR
+
+	if [ ! -f $DATA_DIR/twitter-datasets.zip ];
+	then
+		pushd $DATA_DIR
+
+		wget -c http://www.da.inf.ethz.ch/files/twitter-datasets.zip
+		unzip -o twitter-datasets.zip
+
+		popd > /dev/null
+	fi
+	echo "Dataset downloaded."
+}
+
+function download_dependecies() {
+	echo "Downlading dependecies..."
+	pip3 install -r requirements.txt
+	echo "Dependecies downloaded."
+}
+
+pushd $DIR > /dev/null
+download_dataset
+download_dependecies
+popd > /dev/null
