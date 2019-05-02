04003c279adc829bfe1b51991327cc455f5ed6a2
panpan2 <p.panayiotou2@gmail.com>
2019-05-03 00:19:47 +0200

    Introduce globals for all parameters that change

--- util.py
+++ util.py
@@ -52,6 +52,12 @@ def make_final_data(sentences, labels):
 ###########################
 # GLOBAL HYPERPARAMETERS  #
 ###########################
+TRAIN_DATA_BINARY = os.path.join('datasets', 'train-data.bin.npz')
+TEST_DATA_BINARY = os.path.join('datasets', 'test-data.bin.npz')
+POSITIVE_TRAIN_DATA_FILE = os.path.join('datasets', 'twitter-datasets', 'train_pos_full.txt')
+NEGATIVE_TRAIN_DATA_FILE = os.path.join('datasets', 'twitter-datasets', 'train_neg_full.txt')
+TEST_DATA_FILE = os.path.join('datasets', 'twitter-datasets', 'test_data.txt')
+DATA_BINARIES = {True: TRAIN_DATA_BINARY, False: TEST_DATA_BINARY} # not the best convention
 MAX_WORDS = 20000
 MAX_SEQUENCE_LENGTH = 40
 
