275e329079807071c547366818fe2cdb27de70ae
panpan2 <p.panayiotou2@gmail.com>
2019-05-03 00:13:16 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -2,9 +2,6 @@ set -e pipefail
 
 mkdir -p datasets
 cd datasets
-# wget -c http://cs.stanford.edu/people/alecmgo/trainingandtestdata.zip
-# unzip -o trainingandtestdata.zip
 wget -c http://www.da.inf.ethz.ch/files/twitter-datasets.zip
 unzip -o twitter-datasets.zip
 cd ../
-
