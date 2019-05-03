a0ab7d06917e91a2ab444e8600c3eb15e6da8483
panpan2 <p.panayiotou2@gmail.com>
2019-05-03 21:04:14 +0200

    Add train-test split hyperparameter

--- constants.py
+++ constants.py
@@ -1,6 +1,5 @@
 import os
 
-
 TRAIN_DATA_BINARY = os.path.join('datasets', 'train-data.bin.npz')
 TEST_DATA_BINARY = os.path.join('datasets', 'test-data.bin.npz')
 POSITIVE_TRAIN_DATA_FILE = os.path.join('datasets', 'twitter-datasets', 'train_pos_full.txt')
@@ -14,6 +13,9 @@ DATA_BINARIES = {
 
 PREDICTION_FILE = 'test_prediction.csv'
 
+TRAIN_TEST_SPLIT_PERCENTAGE = 0.1
 
+# Used for assigning numbers to most popular words and ignoring the rest
 MAX_WORDS = 20000
-MAX_SEQUENCE_LENGTH = 55
+# Used for padding the smaller tweets
+MAX_SEQUENCE_LENGTH = 50
